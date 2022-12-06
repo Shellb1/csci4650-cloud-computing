bc93c5867ca68b2a43c87ac697c3988ff1df548d
derek-baker-dev <67568439+derek-baker-dev@users.noreply.github.com>
2022-12-06 16:47:17 -0600

    got some stuff set up

--- app.js
+++ app.js
@@ -32,6 +32,63 @@ var server = http.createServer(function (req, res) {
     }
 });
 
+const { get } = require('https');
+var mysql = require('mysql');
+
+connection = mysql.createConnection( {
+    host: "ec2-54-152-168-195.compute-1.amazonaws.com",
+    port: "3306",
+    user: "root",
+    password: "adminStuff",
+    database: "myDB"
+
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+function getTableEntries(name, phone) {
+    connection.query("SELECT * FROM assign4", function (err, result) {
+        if (err) throw err;
+        console.log(result)
+    });
+
+    /*connection.query(`SELECT * FROM entries WHERE name = '${name}' AND 'Phone Number = '${phone}'`, function (err, result) {
+        if (err) throw err;
+        console.log(result)
+    });*/
+
+}
+
+function insertNewEntry() {
+    var sql = "INSERT INTO assign4 VALUES ('TESTING', 'NOPE', 'YUP', '123', 'ANOTHER')";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    })
+}
+
+function removeReservation(name, phone) {
+    var sql = "DELETE FROM assign4 WHERE Title IS NULL";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    })
+
+    /*var sql = `DELETE FROM reservations WHERE name = '${name}' AND 'Phone Number' = '${phone}'`;
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    })*/
+}
+
+getTableEntries();
+//insertNewEntry();
+removeReservation();
+getTableEntries();
+
 // Listen on port 3000, IP defaults to 127.0.0.1
 server.listen(port);
 
